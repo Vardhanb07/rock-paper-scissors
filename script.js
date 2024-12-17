4fc63761fcbea26f0dbf714f500e57e84dbd95a8
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2024-12-17 19:12:56 +0530

    add javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice () {
+    let i = Math.floor(Math.random() * 3);
+    if(i == 0){
+        return "rock";
+    }else if(i == 1){
+        return "paper";
+    }else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
