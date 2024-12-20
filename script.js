3fc24adffaa61f9cbf2f43263f9ab844da2bb8c2
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2024-12-20 19:51:56 +0530

    updated the file

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
+
 function getComputerChoice () {
     let i = Math.floor(Math.random() * 3);
     if(i == 0){
-        return "rock";
+        object = "rock";
     }else if(i == 1){
-        return "paper";
+         object = "paper";
     }else {
-        return "scissors";
+        object = "scissors";
     }
+    return object;
 }
 
-console.log(getComputerChoice());
+let choice;
+
+function getHumanChoice(){
+    choice = prompt("Enter your choice : ");
+    choice = choice.toLowerCase();
+    return choice;
+}
+
+function playGame(){
+
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanSelection, computerSelection){
+
+    //Human score calculation
+
+    //Win
+
+    if(humanSelection == 'rock' && computerSelection == 'scissors'){
+        console.log(`You win! $(humanSelection) beats $(computerSelection)`);
+        humanScore++;
+    }else if(humanSelection == 'paper' && computerSelection == 'rock'){
+        console.log(`You win! $(humanSelection) beats $(computerSelection)`);
+        humanScore++;
+    }else if(humanSelection == 'scissors' && computerSelection == 'paper'){
+        console.log(`You win! $(humanSelection) beats $(computerSelection)`);
+        humanScore++;
+    }
+
+    //Lose
+
+    if(humanSelection == 'rock' && computerSelection == 'paper'){
+        console.log(`You lose! $(humanSelection) beats $(computerSelection)`);
+        computerScore++;
+    }else if(humanSelection == 'paper' && computerSelection == 'scissors'){
+        console.log(`You lose! $(humanSelection) beats $(computerSelection)`);
+        computerScore++;
+    }else if(humanSelection == 'scissors' && computerSelection == 'rock'){
+        console.log(`You lose! $(humanSelection) beats $(computerSelection)`);
+        computerScore++;
+    }
+
+}
+
+}
+
+for(let i = 0; i < 5; i++){
+    playGame();
+}
