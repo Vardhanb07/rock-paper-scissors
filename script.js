853749dbdbf937b4f564f6dbeb46b630db858d2c
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-01-14 12:59:03 +0530

    Added event listeners to the images for updating scores

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt('Enter your choice : ');
-    choice = choice.toLowerCase();
-    return choice;
-}
+// function getHumanChoice(){
+//     let choice = prompt('Enter your choice : ');
+//     choice = choice.toLowerCase();
+//     return choice;
+// }
 
 let computerScore = 0;
 let humanScore = 0;
@@ -21,37 +21,37 @@ let humanScore = 0;
 function playGame(x,y){
     //win
     if (x === 'rock' && y === 'scissors'){
-        console.log(`You win! ${x} beats ${y}`);
+        // console.log(`You win! ${x} beats ${y}`);
         humanScore++;
     }else if(x === 'paper' && y === 'rock'){
-        console.log(`You win! ${x} beats ${y}`);
+        // console.log(`You win! ${x} beats ${y}`);
         humanScore++;
     }else if(x === 'scissors' && y === 'paper'){
-        console.log(`You win! ${x} beats ${y}`);
+        // console.log(`You win! ${x} beats ${y}`);
         humanScore++;
     }
     //lose
     else if(x === 'rock' && y === 'paper'){
-        console.log(`You lose! ${y} beats ${x}`);
+        //console.log(`You lose! ${y} beats ${x}`);
         computerScore++;
     }else if(x === 'paper' && y === 'scissors'){
-        console.log(`You lose! ${y} beats ${x}`);
+        //console.log(`You lose! ${y} beats ${x}`);
         computerScore++;
     }else if(x === 'scissors' && y === 'rock'){
-        console.log(`You lose! ${y} beats ${x}`);
+        //console.log(`You lose! ${y} beats ${x}`);
         computerScore++;
     }
     //tie
     else if(x === y){
-        console.log("It's a tie!");
+        //console.log("It's a tie!");
     }
 }
 
-function playRound(){
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    return playGame(humanChoice,computerChoice);
-}
+// function playRound(){
+//     let humanChoice = getHumanChoice();
+//     let computerChoice = getComputerChoice();
+//     return playGame(humanChoice,computerChoice);
+// }
 
 // for(let i = 0; i < 5; i++){
 //     playRound();
@@ -60,36 +60,49 @@ function playRound(){
 // console.log("Your score : " + humanScore);
 // console.log("Computer score : " + computerScore);
 
+const hScore = document.querySelector('.humanScore');
+const cScore = document.querySelector('.computerScore');
 //Shows human choice onto the screen 
 const  hChoice = document.querySelector('.humanChoice');
 //Rock 
 const rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
     hChoice.textContent = 'Your choice : Rock';
+    playGame('rock',getComputerChoice());
+    hScore.textContent = `Your score : ${humanScore}`;
+    cScore.textContent = `Computer score : ${computerScore}`;
 })
 //Paper
 const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
     hChoice.textContent = 'Your choice : Paper';
+    playGame('paper',getComputerChoice());
+    hScore.textContent = `Your score : ${humanScore}`;
+    cScore.textContent = `Computer score : ${computerScore}`;
 })
 //Scissors
 const scissors = document.querySelector('.scissors');
 scissors.addEventListener('click', () => {
     hChoice.textContent = 'Your choice : Scissors';
+    playGame('scissors',getComputerChoice());
+    hScore.textContent = `Your score : ${humanScore}`;
+    cScore.textContent = `Computer score : ${computerScore}`;
 })
-
 //Shows computer choice onto the screen
 const cChoice = document.querySelector('.computerChoice');
 rock.addEventListener('click', () => {
-    cChoice.textContent = `Computer choice : ${getComputerChoice()}`;  
+    cChoice.textContent = `Computer choice : ${getComputerChoice()}`; 
+    console.log(getComputerChoice()); 
 })
 
 paper.addEventListener('click', () => {
     cChoice.textContent = `Computer choice : ${getComputerChoice()}`;  
+    console.log(getComputerChoice());
 })
 
 scissors.addEventListener('click', () => {
     cChoice.textContent = `Computer choice : ${getComputerChoice()}`;  
+    console.log(getComputerChoice());
 })
 
 
