19739ffcf43821b607f3e1563b57fd67bf5af3fa
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-01-17 12:48:00 +0530

    Add logic of the game, changes will be made

--- script.js
+++ script.js
@@ -1,110 +1,123 @@
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if(num == 0){
-        return 'rock';
+        return 'Rock';
     }else if(num == 1){
-        return 'paper';
+        return 'Paper';
     }else if(num == 2){
-        return 'scissors';
+        return 'Scissors';
     }
 }
-
-// function getHumanChoice(){
-//     let choice = prompt('Enter your choice : ');
-//     choice = choice.toLowerCase();
-//     return choice;
-// }
 
 let computerScore = 0;
 let humanScore = 0;
 
 function playGame(x,y){
     //win
-    if (x === 'rock' && y === 'scissors'){
-        // console.log(`You win! ${x} beats ${y}`);
+    if (x === 'Rock' && y === 'Scissors'){
         humanScore++;
-    }else if(x === 'paper' && y === 'rock'){
-        // console.log(`You win! ${x} beats ${y}`);
+    }else if(x === 'Paper' && y === 'Rock'){
         humanScore++;
-    }else if(x === 'scissors' && y === 'paper'){
-        // console.log(`You win! ${x} beats ${y}`);
+    }else if(x === 'Scissors' && y === 'Paper'){
         humanScore++;
     }
     //lose
-    else if(x === 'rock' && y === 'paper'){
-        //console.log(`You lose! ${y} beats ${x}`);
+    else if(x === 'Rock' && y === 'Paper'){
         computerScore++;
-    }else if(x === 'paper' && y === 'scissors'){
-        //console.log(`You lose! ${y} beats ${x}`);
+    }else if(x === 'Paper' && y === 'Scissors'){
         computerScore++;
-    }else if(x === 'scissors' && y === 'rock'){
-        //console.log(`You lose! ${y} beats ${x}`);
+    }else if(x === 'Scissors' && y === 'Rock'){
         computerScore++;
     }
-    //tie
-    // else if(x === y){
-    //     //console.log("It's a tie!");
-    // }
 }
 
-// function playRound(){
-//     let humanChoice = getHumanChoice();
-//     let computerChoice = getComputerChoice();
-//     return playGame(humanChoice,computerChoice);
-// }
+function reset() {
+    humanChoice.textContent = 'Your choice : None';
+    computerChoice.textContent = 'Computer choice : None';
+    humanScoreInText.textContent = 'Your score : 0';
+    computerScoreInText.textContent = 'Computer score : 0';
+    humanScore = 0;
+    computerScore = 0;
+}
 
-// for(let i = 0; i < 5; i++){
-//     playRound();
-// }
+//x : human information, y : computer information
 
-// console.log("Your score : " + humanScore);
-// console.log("Computer score : " + computerScore);
+function choiceText(x, y){
+    humanChoice.textContent = `Your choice : ${x}`;
+    computerChoice.textContent = `Computer choice : ${y}`;
+}
 
-const hScore = document.querySelector('.humanScore');
-const cScore = document.querySelector('.computerScore');
-//Shows human choice onto the screen 
-const  hChoice = document.querySelector('.humanChoice');
-//Rock 
-const cChoice = document.querySelector('.computerChoice');
+function scoreText(x, y){
+    humanScoreInText.textContent = `Your score : ${x}`;
+    computerScoreInText.textContent = `Computer score : ${y}`;
+}
+
+const humanChoice = document.querySelector('.humanChoice');
+const computerChoice = document.querySelector('.computerChoice');
+const humanScoreInText = document.querySelector('.humanScore');
+const computerScoreInText = document.querySelector('.computerScore');
+const feedback = document.querySelector('.feedbackPrompt');
+let count = 0;
+
 const rock = document.querySelector('.rock');
-let cc = getComputerChoice();
+
 rock.addEventListener('click', () => {
-    hChoice.textContent = 'Your choice : Rock';
-    playGame('rock',cc);
-    hScore.textContent = `Your score : ${humanScore}`;
-    // cScore.textContent = `Computer score : ${cc}`;
-    cc = getComputerChoice();
-})
-console.log(cc);
-//Paper
+    let cc = getComputerChoice();
+    playGame('Rock', cc);
+    choiceText('Rock', cc);
+    scoreText(humanScore, computerScore);
+});
+
+rock.addEventListener('click', () => {
+    count++;
+});
+
 const paper = document.querySelector('.paper');
+
 paper.addEventListener('click', () => {
-    hChoice.textContent = 'Your choice : Paper';
-    playGame('paper',getComputerChoice());
-    hScore.textContent = `Your score : ${humanScore}`;
-    cScore.textContent = `Computer score : ${computerScore}`;
-    cChoice.textContent = `Computer choice : ${getComputerChoice()}`;
-})
-//Scissors
+    let cc = getComputerChoice();
+    playGame('Paper', cc);
+    choiceText('Paper', cc);
+    scoreText(humanScore, computerScore);
+});
+
+paper.addEventListener('click', () => {
+    count++;
+    console.log(count);
+});
+
 const scissors = document.querySelector('.scissors');
-scissors.addEventListener('click', () => {
-    hChoice.textContent = 'Your choice : Scissors';
-    playGame('scissors',getComputerChoice());
-    hScore.textContent = `Your score : ${humanScore}`;
-    cScore.textContent = `Computer score : ${computerScore}`;
-})
-// Shows computer choice onto the screen
-rock.addEventListener('click', () => {
-    cChoice.textContent = `Computer choice : ${cc}`; 
-    console.log(cc); 
-})
-
-paper.addEventListener('click', () => {
-    cChoice.textContent = `Computer choice : ${getComputerChoice()}`;  
-    console.log(getComputerChoice());
-})
 
 scissors.addEventListener('click', () => {
-    cChoice.textContent = `Computer choice : ${getComputerChoice()}`;  
-    console.log(getComputerChoice());
-})
+    let cc = getComputerChoice();
+    playGame('Scissors', cc);
+    choiceText('Scissors', cc);
+    scoreText(humanScore, computerScore);
+});
+
+scissors.addEventListener('click', () => {
+    count++;
+    console.log(count);
+});
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click',() => {
+    if(count >= 5){
+        if(computerScore < humanScore){
+            feedback.textContent = 'You win!';
+            reset();
+        }else{
+            feedback.textContent = 'You lose!';
+            reset();
+        }
+    }
+});
+
+const resetInfo = document.querySelector('.reset');
+
+resetInfo.addEventListener('click', () => {
+    reset();
+    feedback.textContent = '';
+});
+
